4e77a850db30bcdb6786ec41e339592c890e3cd6
Richard Lichkus <rpl5018@gmail.com>
2014-08-14 13:05:21 -0700

    Initial Commit

---
+++ CKGestureKit/CKViewController.h
@@ -0,0 +1,13 @@
+//
+//  CKViewController.h
+//  CKGestureKit
+//
+//  Created by Richard Lichkus on 8/14/14.
+//  Copyright (c) 2014 Richard Lichkus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CKViewController : UIViewController
+
+@end
