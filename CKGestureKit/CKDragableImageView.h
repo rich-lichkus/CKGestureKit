601dc4fd97bbcc593319444c032b79c55186a970
Richard Lichkus <rpl5018@gmail.com>
2014-08-15 10:45:13 -0700

    Dragable View

---
+++ CKGestureKit/CKDragableImageView.h
@@ -0,0 +1,13 @@
+//
+//  CKDragableImageView.h
+//  CKGestureKit
+//
+//  Created by Richard Lichkus on 8/14/14.
+//  Copyright (c) 2014 Richard Lichkus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CKDragableImageView : UIImageView
+
+@end
---
+++ CKGestureKit/CKDragableImageView.m
@@ -0,0 +1,31 @@
+//
+//  CKDragableImageView.m
+//  CKGestureKit
+//
+//  Created by Richard Lichkus on 8/14/14.
+//  Copyright (c) 2014 Richard Lichkus. All rights reserved.
+//
+
+#import "CKDragableImageView.h"
+
+@implementation CKDragableImageView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
